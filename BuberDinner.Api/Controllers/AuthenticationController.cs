dde67f858b69c57e2bea78181088ac36e3122a6f
sfilipovski <filipovskisergej@gmail.com>
2023-12-17 22:31:29 +0100

    initial commit

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
